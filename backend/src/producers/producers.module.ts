11b70c38915967aae43c84ad66ed0de0a65ae8de
Pablo Werner <pablo.pozzer@gmail.com>
2026-03-30 17:40:36 -0300

    fix(backend): inject PrismaService into Producers and Transactions modules resolving NestJS startup crash

--- backend/src/producers/producers.module.ts
+++ backend/src/producers/producers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProducersController } from './producers.controller';
 import { ProducersService } from './producers.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [ProducersController],
-  providers: [ProducersService]
+  providers: [ProducersService, PrismaService]
 })
 export class ProducersModule {}
--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [TransactionsController],
-  providers: [TransactionsService]
+  providers: [TransactionsService, PrismaService]
 })
 export class TransactionsModule {}
